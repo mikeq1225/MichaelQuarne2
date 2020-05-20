00435e7dd7d4ce0ae22cc5d4bc83a94d5a5ab996
Michael Quarne <mikedq1225@gmail.com>
2020-05-20 15:17:39 -0700

    trying new routes

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -2,7 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 router.get("*", (req, res, next) => {
-  res.json({ message: "hello world" })
+	res.json({ message: "hello world" })
+})
+
+router.get("/projects", (req, res, next) => {
+	res.json(projects)
 })
 
 module.exports = router
