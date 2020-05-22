fb5758e924b25df9c0e8f29545c75e124327ef0b
Michael Quarne <mikedq1225@gmail.com>
2020-05-22 14:54:03 -0700

    add lazy load

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,30 @@
-import React from "react"
+import React, { Suspense } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./Home"
 import AboutMe from "./AboutMe"
 import Projects from "./projects/Projects"
-import ProjectView from "./projects/ProjectView"
+// import ProjectView from "./projects/ProjectView"
 import Education from "./Education"
 import SocialBar from "./SocialBar"
 import NavBar from "./NavBar"
 import Resume from "./Resume"
 
+const ProjectView = React.lazy(() => import("./projects/ProjectView"))
+
 export default (props) => {
 	return (
 		<Router>
 			<div>
-				<Route exact path="/" component={Home}></Route>
-				<Route path="/" component={SocialBar}></Route>
-				<Route path="/" component={NavBar}></Route>
-				<Route path="/About" component={AboutMe}></Route>
-				<Route path="/Resume" component={Resume}></Route>
-				<Route exact path="/Projects" component={Projects}></Route>
-				<Route path="/Education" component={Education}></Route>
-				{/* Try lazy load for project view */}
-				<Route path="/Projects/:id" component={ProjectView}></Route>
+				<Suspense fallback={<div>Loading...</div>}>
+					<Route exact path="/" component={Home}></Route>
+					<Route path="/" component={SocialBar}></Route>
+					<Route path="/" component={NavBar}></Route>
+					<Route path="/About" component={AboutMe}></Route>
+					<Route path="/Resume" component={Resume}></Route>
+					<Route exact path="/Projects" component={Projects}></Route>
+					<Route path="/Education" component={Education}></Route>
+					<Route path="/Projects/:id" component={ProjectView}></Route>
+				</Suspense>
 			</div>
 		</Router>
 	)
